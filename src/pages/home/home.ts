d79593c4d5129cbd2b996fae1c5a940f37e6eb40
j <bo_olk@outlook.com>
2018-03-25 23:08:30 +0700

    this test commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  contacts = [
+    {name : "Traning",phone:"081-4545540"},
+    {name : "Support",phone:"081-4545541"},
+    {name : "Customer Service",phone:"081-4545542"}
+  ]
   constructor(public navCtrl: NavController) {
 
   }
+
+  showDetail(contact){
+    //alert(contact.name)
+    this.navCtrl.push("DetailPage",contact)
+  }
+
 
 }
